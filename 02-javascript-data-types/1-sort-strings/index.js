a57e5102af248fc23471f4fc6973d8644732e787
dariasamo <daria@dariasamo.com>
2023-03-01 17:32:45 +0100

    Add sorting function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,24 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
+  function sortByLocaleAndCase(a, b) {
+    const result = a.localeCompare(b, "ru", { sensitivity: "base" });
+    if (result !== 0) {
+      return result;
+    } else {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    }
+  }
+  const newArr = [...arr].sort(sortByLocaleAndCase);
+  if (param === "desc") {
+    newArr.reverse();
+  }
+  return newArr;
 }
