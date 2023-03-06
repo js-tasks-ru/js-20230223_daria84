27c5f999daba36ce84a2215919828cf0a76780c0
dariasamo <daria@dariasamo.com>
2023-03-06 18:10:30 +0100

    Move path array outside of child function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,7 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArr = path.split(".");
   return function (obj) {
-    return path.split(".").reduce((acc, prop) => acc && acc[prop], obj);
+    return pathArr.reduce((acc, prop) => acc && acc[prop], obj);
   };
 }
